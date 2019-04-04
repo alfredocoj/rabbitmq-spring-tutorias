01e15cc3808de34df25fae08cb2dc149d1880c18
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-04-04 08:23:21 -0300

    codigo fonte dos tutoriais

---
+++ src/main/java/br/com/ithappens/RabbitAmqpRunner.java
@@ -0,0 +1,21 @@
+package br.com.ithappens;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class RabbitAmqpRunner implements CommandLineRunner {
+
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    public void run(String... arg0) throws Exception {
+        System.out.println("Ready ... running for " + duration + "ms");
+        Thread.sleep(duration);
+        ctx.close();
+    }
+}
